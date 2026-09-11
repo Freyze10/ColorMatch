80e081c73fb09bafa054e3a90e6fcf89c87d2666
Freyze10 <bjabillanoza@gmail.com>
2026-09-11 13:41:05 +0800

    fix the model default

--- core/main/models.py
+++ core/main/models.py
@@ -367,7 +367,7 @@ class tbl_cmf_pending_completed(models.Model):
     )
     # References tbl_rs.rs_id ()
     rs_no = models.ForeignKey('tbl_rs', on_delete=models.SET_NULL, blank=True, null=True, db_column='rs_id')
-    reason = models.TextField(blank=True, null=True)
+    reason = models.TextField(blank=True, null=True, default='Pending')
     code_details = models.TextField(blank=True, null=True)
     code = models.ForeignKey(
         'tbl_generated_prod_code', 
