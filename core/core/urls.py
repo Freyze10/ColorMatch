6a0fe823dbb82f55a23fb18f53b6ec17f0801a62
Freyze10 <bjabillanoza@gmail.com>
2026-09-15 10:49:52 +0800

    fixed the load of the manifest.js so the installation of the app is a standalone

--- core/core/urls.py
+++ core/core/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     #  two paths for PWA
-    path('sw.js', TemplateView.as_view(template_name='sw.js', content_type='application/javascript')),
-    path('manifest.json', TemplateView.as_view(template_name='manifest.json', content_type='application/json')),
+    path('sw.js', TemplateView.as_view(template_name='PWA/sw.js', content_type='application/javascript')),
+    path('manifest.json', TemplateView.as_view(template_name='PWA/manifest.json', content_type='application/manifest+json')),
 
     path('', include("main.urls")),
 ]
