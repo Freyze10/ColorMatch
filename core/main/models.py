bd5af81ae60ecc92125a22af31aea4cc1402c6e1
Freyze10 <bjabillanoza@gmail.com>
2026-09-22 10:49:30 +0800

    chaged the limit of the qty per order

--- core/main/models.py
+++ core/main/models.py
@@ -247,7 +247,7 @@ class tbl_cmf(models.Model):
         null=True
     )
     est_qty_order = models.DecimalField(
-        max_digits=6, 
+        max_digits=10, 
         decimal_places=2, 
         blank=True, 
         null=True,
