f813184760f9372f8e3cc596ca7fff3b16168b0a
Freyze10 <bjabillanoza@gmail.com>
2026-09-24 15:33:48 +0800

    updated the display of the version

--- core/main/context_processors.py
+++ core/main/context_processors.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 def sidebar_permissions(request):
     if not request.user.is_authenticated or not getattr(request.user, 'role', None):
         return {'user_permissions': set()}
@@ -8,3 +10,8 @@ def sidebar_permissions(request):
         .values_list('access__access_name', flat=True)
     )
     return {'user_permissions': enabled}
+
+def app_version(request):
+    return {
+        "APP_VERSION": settings.APP_VERSION
+    }
