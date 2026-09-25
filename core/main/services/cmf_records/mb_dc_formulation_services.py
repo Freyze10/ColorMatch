d45461ebf3f2e7322eef353942b4fd374bb6b37c
Freyze10 <bjabillanoza@gmail.com>
2026-09-25 12:05:50 +0800

    capitalize the data forr the mb formula parent

--- core/main/services/cmf_records/mb_dc_formulation_services.py
+++ core/main/services/cmf_records/mb_dc_formulation_services.py
@@ -124,11 +124,11 @@ def get_formulation_details(request):
     data = {
         'customer': formula_info.customer if formula_info else "",
         'resin': resin_str,
-        'color': cmf.color_desc or (cmf.in_code_no.color or ""),
+        'color': cmf.color_desc.upper() or (cmf.in_code_no.color or ""),
         'product_code': generated_code if generated_code else (cmf.in_code_no.code if cmf.in_code_no else ""),
         'dosage': formula_info.dosage if formula_info else "",
         'lot_no': generated_lot,
-        'application': app_str,
+        'application': app_str.upper() if app_str else "",
         'finished_product': formula_info.finished_product if formula_info else "",
         'colorant_type': colorant_type,
     }
