f1c7a26f53f27d92a1738477a6b7a16a94999fd4
Freyze10 <bjabillanoza@gmail.com>
2026-09-25 11:49:17 +0800

    adde an ajax for the formula records to check the duplicate lotno

--- core/main/urls.py
+++ core/main/urls.py
@@ -53,6 +53,7 @@ urlpatterns = [
     path('cmf/rs/get-final-code/', rs_records_services.get_cmf_final_code, name='get_cmf_final_code'),
     path('cmf/records/data/', cmf_records_services.cmf_records_data, name='cmf_records_data'),
     path('cmf/mb-dc-formula/', mb_dc_formulation_services.get_formulation_details, name='mb_dc_lookup_details'),
+    path('cmf/mb-dc-formula/check-lot-number/', mb_dc_formulation_services.check_lot_number, name='check_lot_number'),
     path('cmf/formula-records/data/', cmf_records_services.formula_records_data, name='formula_records_data'),
     path('cmf/formula-records/price-first/', formula_price_first.get_price_first_data, name='get_price_first_data'),
     path('cmf/formula-records/price-first/download/', formula_price_first.download_price_first_excel, name='download_price_first_excel'),
